a8489a0027e0bbbe1fc438428288adcb197fb60a
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-28 23:17:18 +0100

    refactor the name of main class

--- src/main/java/guru/springframework/sdjpaintro/SdJPAIntroApplication.java
+++ src/main/java/guru/springframework/sdjpaintro/SdJPAIntroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SdjpaIntroApplication {
+public class SdJPAIntroApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SdjpaIntroApplication.class, args);
+        SpringApplication.run(SdJPAIntroApplication.class, args);
     }
 
 }
