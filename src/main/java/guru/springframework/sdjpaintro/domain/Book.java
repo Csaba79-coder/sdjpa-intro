9027d4f37778a92e452095d928e800004c790dea
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-28 23:40:09 +0100

    creating Book as a JPA Entity

---
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -0,0 +1,59 @@
+package guru.springframework.sdjpaintro.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    // the package called domain or model or entity
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    // @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
