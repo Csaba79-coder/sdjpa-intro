18620b4d2b3507a71a071d66f1280ed989061fd9
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-29 14:06:44 +0100

    create SpringBoot test for BookRepository

--- src/test/java/guru/springframework/sdjpaintro/SdJPAIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdJPAIntroApplicationTests.java
@@ -1,10 +1,25 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SdJPAIntroApplicationTests {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testBookRepository() {
+        Long count = bookRepository.count();
+        assertThat(count).isGreaterThan(0);
+        assertEquals(count, 2); // as far as I had 2 example books created!
+    }
 
     @Test
     void contextLoads() {
